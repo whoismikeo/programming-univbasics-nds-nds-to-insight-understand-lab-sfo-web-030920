5ca74b63ee7e77714a83f6a798a9fe9eca5467ee
Michael Ochnicki <ochnicki.michael@gmail.com>
2020-02-18 01:46:12 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -18,7 +18,7 @@ def print_first_directors_movie_titles
   for el in directors_database[0][:movies]
     list += el[:title] + '\n'
   end
-  list
+  puts list
 end
 
 print_first_directors_movie_titles
